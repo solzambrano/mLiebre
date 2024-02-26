4e7d109a44133f92a49d72e15986add9ff1111a6
solzambrano <solzambrano.8@gmail.com>
2024-02-26 10:13:15 -0300

    app y package

---
+++ app.js
@@ -0,0 +1,12 @@
+const express= require('express');
+const path=require('path');
+const app= express();
+const pathPublic=path.resolve(__dirname,'./public')
+app.use(express.static(pathPublic))
+app.listen(3000,()=>{
+    console.log('puerto andando')
+})
+
+app.get('/',(req,res)=>{
+res.sendFile(path.resolve(__dirname,'./views/home.html'))
+})
