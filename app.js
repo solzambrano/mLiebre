cf5427bc1f71ff85f26147d63c7cee9dad4ec8df
solzambrano <solzambrano.8@gmail.com>
2024-02-29 08:33:04 -0300

    primer disenio

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ const express= require('express');
 const path=require('path');
 const app= express();
 const port=process.env.PORT || 3000
+const pathPublic=path.resolve(__dirname,'./public')
+
 app.listen(port,()=>{
     console.log( `servidor corriendo ${port}`)
 })
-const pathPublic=path.resolve(__dirname,'./public')
 app.use(express.static(pathPublic))
-app.listen(3000,()=>{
+
+app.listen(3030,()=>{
     console.log('puerto andando')
 })
-
+// D:\SOL - Backup Notebook\Datos\proyectosol\mercadoLiebre\views\formulario.html
 app.get('/',(req,res)=>{
+    console.log(__dirname)
 res.sendFile(path.resolve(__dirname,'./views/home.html'))
+})
+
+app.get('/register',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/register.html'))
 })
