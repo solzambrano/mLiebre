e85a5ac8d044aac24421c10a4f91d1768b67989e
solzambrano <solzambrano.8@gmail.com>
2024-03-29 17:06:36 -0300

    primeras configuraciones

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ const path=require('path');
 const app= express();
 const port=process.env.PORT || 3000
 const pathPublic=path.resolve(__dirname,'./public')
+const router=express.Router();
 
 app.listen(port,()=>{
     console.log( `servidor corriendo ${port}`)
 })
 app.use(express.static(pathPublic))
+app.set('view engine','ejs')
 
 app.listen(3030,()=>{
     console.log('puerto andando')
 })
 // D:\SOL - Backup Notebook\Datos\proyectosol\mercadoLiebre\views\formulario.html
-app.get('/',(req,res)=>{
-    console.log(__dirname)
-res.sendFile(path.resolve(__dirname,'./views/home.html'))
-})
+// app.get('/',(req,res)=>{
+// res.sendFile(path.resolve(__dirname,'./views/home.html'))
+// })
 
 app.get('/register',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/register.html'))
---
+++ controller/controller.js
@@ -0,0 +1,7 @@
+const controller={
+    index:function(req,res){
+        res.send('hola')
+    }
+}
+
+module.exports=controller
